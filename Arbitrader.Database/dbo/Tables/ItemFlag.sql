3c21822a843670f948a618e8e8f004caa156221c
Fabrosi <bunsofwrath@users.noreply.github.com>
2017-09-07 23:13:21 -0500

    Delete ItemFlag.sql

--- Arbitrader.Database/dbo/Tables/ItemFlag.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[ItemFlag]
-(
-	[pk] INT NOT NULL PRIMARY KEY, 
-	[itemPK] INT NULL,
-    [flagPK] INT NULL, 
-	[loadDate] DATETIME2 NOT NULL DEFAULT GETDATE(),
-	CONSTRAINT [FK_ItemFlag_Item] FOREIGN KEY ([itemPK]) REFERENCES [dbo].[Item]([pk]),
-    CONSTRAINT [FK_ItemFlag_Flag] FOREIGN KEY ([flagPK]) REFERENCES [dbo].[Flag]([pk])
-)
